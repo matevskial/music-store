d4f3da06ba4db132b359cecfcff32836a6765aa1
Aleksandar Matevski <matevskial@gmail.com>
2023-07-16 21:11:25 +0200

    Connect to database and read only artists from database

--- src/main/java/com/matevskial/musicstore/Main.java
+++ src/main/java/com/matevskial/musicstore/Main.java
@@ -1,13 +1,34 @@
 package com.matevskial.musicstore;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Main {
 
-  public static void main(String[] args) {
+  private static Connection con;
+
+  public static void main(String[] args) throws ClassNotFoundException, SQLException {
+    initializeDatabaseConnection();
+
     System.out.println("Music store");
     System.out.println("Artists");
     printArtists();
     System.out.println("Songs");
     printSongs();
+
+    deinitializeDatabaseConnection();
+  }
+
+  private static void deinitializeDatabaseConnection() throws SQLException {
+    con.close();
+  }
+
+  private static void initializeDatabaseConnection() throws ClassNotFoundException, SQLException {
+    Class.forName("com.mysql.cj.jdbc.Driver");
+    con = DriverManager.getConnection("jdbc:mysql://localhost:3306/music-store", "root", "12345678");
   }
 
   private static void printSongs() {
@@ -16,9 +37,14 @@ public class Main {
     System.out.println("2 radiactive");
   }
 
-  private static void printArtists() {
-    System.out.println("ID      Name");
-    System.out.println("1 coldplay");
-    System.out.println("2 imagine dragon");
+  private static void printArtists() throws SQLException {
+    try(Statement statement = con.createStatement()) {
+      String query = "select id, name from artists";
+      System.out.println("ID      Name");
+      ResultSet result = statement.executeQuery(query);
+      while(result.next()) {
+        System.out.println(result.getLong("id") + "    " + result.getString("name"));
+      }
+    }
   }
 }
