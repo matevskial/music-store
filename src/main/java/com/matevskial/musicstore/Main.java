b872c8c15907fa8c49afb4e28f7ca163daada09f
Aleksandar Matevski <matevskial@gmail.com>
2023-07-16 20:17:52 +0200

    Display songs and artists using hard-coded data

--- src/main/java/com/matevskial/musicstore/Main.java
+++ src/main/java/com/matevskial/musicstore/Main.java
@@ -4,5 +4,21 @@ public class Main {
 
   public static void main(String[] args) {
     System.out.println("Music store");
+    System.out.println("Artists");
+    printArtists();
+    System.out.println("Songs");
+    printSongs();
+  }
+
+  private static void printSongs() {
+    System.out.println("ID      Name");
+    System.out.println("1 paradise");
+    System.out.println("2 radiactive");
+  }
+
+  private static void printArtists() {
+    System.out.println("ID      Name");
+    System.out.println("1 coldplay");
+    System.out.println("2 imagine dragon");
   }
 }
